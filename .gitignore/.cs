eeec06a8e889db97001e80899761e6ac95726008
Resul Önder <resul_199271@hotmail.com>
2018-04-18 16:01:48 +0300

    Update .cs

--- .gitignore/.cs
+++ .gitignore/.cs
@@ -2,6 +2,8 @@
  
  private void timer1_Tick(object sender, EventArgs e)
         {
+  //budara label yerine buton panel vb neslerer referans verilerek nesnelerde kaydırılabilir
+  
             if (label1.Left < -label1.Width)//kayacagı boyut
             {
                 label1.Left = 600;//Tekrar başlayacagı konum
