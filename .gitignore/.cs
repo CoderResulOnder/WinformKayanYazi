c6333a1170e0f2448a8908cd93b033e9ccebb1ef
Resul Önder <resul_199271@hotmail.com>
2018-04-18 16:00:56 +0300

    Update .cs

--- .gitignore/.cs
+++ .gitignore/.cs
@@ -2,12 +2,14 @@
  
  private void timer1_Tick(object sender, EventArgs e)
         {
-            if (label1.Left < -label1.Width)
+            if (label1.Left < -label1.Width)//kayacagı boyut
             {
-                label1.Left = 600;
+                label1.Left = 600;//Tekrar başlayacagı konum
             }
             else
             {
-                label1.Left -= 3;
+                label1.Left -= 3;//1 ms hareket ne kadar
             }
         }
+
+
